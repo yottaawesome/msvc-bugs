7ee5665b8a71262c617c40780f29b4a70f53bb45
Vasilios Magriplis <etameson@gmail.com>
2023-06-11 10:13:52 +1000

    Remove some fluff frm SourceLocationDefaultParam

--- src/SourceLocationDefaultParam/main.cpp
+++ src/SourceLocationDefaultParam/main.cpp
@@ -1,19 +1,15 @@
-#include <source_location> // uncomment to fix ICE
+// #include <source_location> // uncomment to fix ICE
 //import <source_location>; // does not fix ICE
 //import std; // does not fix ICE
 import testexception;
 
 int main()
 {
-    try
-    {
-        // ICE only occurs when constructed and thrown in one statement. 
-        // ICE doesn't occur when separately constructing and then
-        // throwing the exception.
-        // ICE doesn't occur when invoking functions or function members.
-        throw TestExceptions::Exception("Some test message");
-    }
-    catch (...) { }
+    // ICE only occurs when constructed and thrown in one statement. 
+    // ICE doesn't occur when separately constructing and then
+    // throwing the exception.
+    // ICE doesn't occur when invoking functions or function members.
+    throw TestExceptions::Exception("Some test message");
 
     return 0;
 }
--- src/SourceLocationDefaultParam/testexception.ixx
+++ src/SourceLocationDefaultParam/testexception.ixx
@@ -5,13 +5,11 @@ import <source_location>;
 
 export namespace TestExceptions
 {
-	class Exception : public std::exception
+	struct Exception : public std::exception
 	{
-		public:
-			virtual ~Exception() = default;
-			Exception(
-				const char* msg,
-				const std::source_location& location = std::source_location::current()
-			) {}
+		Exception(
+			const char* msg,
+			const std::source_location& location = std::source_location::current()
+		) {}
 	};
 }
