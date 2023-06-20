45ee42e1bfc800c8ae15c49f2d0cc7a7c89c5361
Vasilios Magriplis <etameson@gmail.com>
2023-06-20 17:36:57 +1000

    Minor changes

--- src/module-debug-break/main.cpp
+++ src/module-debug-break/main.cpp
@@ -4,10 +4,7 @@ import somemodule;
 int main()
 {
     SomeNamespace::SomeObject obj;
-    // This doesn't work. We expect to get back "blahblahblahblah", 
-    // but instead we get garbage back, and the program crashes 
-    // when the scope ends in Debug builds.
     std::string s = obj.DoSomething();
 
-    return 0;
+    return 0; // place breakpoint here
 }
