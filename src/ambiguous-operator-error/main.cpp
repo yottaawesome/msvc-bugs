1d53dd0c823c460751029fb2cb35d556b5107c0a
Vasilios Magriplis <etameson@gmail.com>
2026-03-12 10:21:32 +1100

    Minor touch ups.

--- src/ambiguous-operator-error/main.cpp
+++ src/ambiguous-operator-error/main.cpp
@@ -1,7 +1,50 @@
 import std;
 
+namespace strings
+{
+	template<typename T>
+	struct always_false : std::false_type {};
+
+	template<std::size_t N, typename TChar = char>
+	struct fixed_string
+	{
+		TChar Buffer[N]{};
+		constexpr fixed_string(const TChar(&str)[N])
+		{
+			std::copy_n(str, N, Buffer);
+		}
+		constexpr auto begin(this auto&& self) noexcept { return self.Buffer; }
+		constexpr auto end(this auto&& self) noexcept { return self.Buffer + N - 1; }
+
+		template<std::size_t M>
+		constexpr auto operator==(this auto&& self, const TChar(&str)[M]) noexcept -> bool
+		{
+			if constexpr (N != M)
+				return false;
+			else
+				return std::equal(self.begin(), self.end(), str, str + N - 1);
+		}
+
+		template<std::size_t M>
+		constexpr auto operator==(this auto&& self, const fixed_string<M, TChar>& other) noexcept -> bool
+		{
+			if constexpr (N != M)
+				return false;
+			else
+				return std::equal(self.begin(), self.end(), other.begin(), other.end());
+		}
+	};
+	template<std::size_t N>
+	fixed_string(const char(&str)[N]) -> fixed_string<N, char>;
+	template<std::size_t N>
+	fixed_string(const wchar_t(&str)[N]) -> fixed_string<N, wchar_t>;
+	// GCC and Clang in C++26 mode compile this code with a warning, but MSVC fails
+	// with "C2666: 'operator==': overloaded operator have similar conversions".
+	static_assert(fixed_string("Hello") == fixed_string("Hello"));
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	return 0;
 }
 
